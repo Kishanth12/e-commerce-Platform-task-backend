f8d65d7d40220a987a58d249c6893eed02629989
kishanth12 <kishanth1209@gmail.com>
2025-12-15 21:45:16 +0530

    chore: setup Express server with routes and MongoDB connection

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import productRouter from "./routes/product.routes.js";
+import orderRouter from "./routes/order.routes.js";
+
+dotenv.config();
+connectDb();
+
+const app = express();
+
+app.use("/api/users", authRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
